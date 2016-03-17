5df158ec93e2cba27a8bba5cf6cfa817bd82fa3a
Socheat KHAUV <pkayjava@gmail.com>
2016-03-17 23:05:42 +0700

    attribute name format create on website

--- mbaas-server/src/main/java/com/angkorteam/mbaas/server/validator/AttributeNameValidator.java
+++ mbaas-server/src/main/java/com/angkorteam/mbaas/server/validator/AttributeNameValidator.java
@@ -1,11 +1,14 @@
 package com.angkorteam.mbaas.server.validator;
 
 import com.angkorteam.framework.extension.share.validation.JooqValidator;
+import com.angkorteam.mbaas.configuration.Constants;
 import com.angkorteam.mbaas.model.entity.Tables;
 import com.angkorteam.mbaas.model.entity.tables.AttributeTable;
 import org.apache.wicket.validation.IValidatable;
 import org.apache.wicket.validation.ValidationError;
 import org.jooq.DSLContext;
+
+import java.util.regex.Pattern;
 
 /**
  * Created by socheat on 3/8/16.
@@ -29,18 +32,22 @@ public class AttributeNameValidator extends JooqValidator<String> {
     public void validate(IValidatable<String> validatable) {
         String name = validatable.getValue();
         if (name != null && !"".equals(name)) {
-
-            AttributeTable attributeTable = Tables.ATTRIBUTE.as("attributeTable");
-
-            DSLContext context = getDSLContext();
-            int count = 0;
-            if (attributeId == null || "".equals(attributeId)) {
-                count = context.selectCount().from(attributeTable).where(attributeTable.NAME.eq(name)).and(attributeTable.COLLECTION_ID.eq(collectionId)).fetchOneInto(int.class);
+            Pattern patternNaming = Pattern.compile(Constants.getXmlPropertiesConfiguration().getString(Constants.PATTERN_NAMING));
+            if (!patternNaming.matcher(name).matches()) {
+                validatable.error(new ValidationError(this, "format"));
             } else {
-                count = context.selectCount().from(attributeTable).where(attributeTable.NAME.eq(name)).and(attributeTable.COLLECTION_ID.eq(this.collectionId)).and(attributeTable.ATTRIBUTE_ID.ne(attributeId)).fetchOneInto(int.class);
-            }
-            if (count > 0) {
-                validatable.error(new ValidationError(this, "duplicated"));
+                AttributeTable attributeTable = Tables.ATTRIBUTE.as("attributeTable");
+
+                DSLContext context = getDSLContext();
+                int count = 0;
+                if (attributeId == null || "".equals(attributeId)) {
+                    count = context.selectCount().from(attributeTable).where(attributeTable.NAME.eq(name)).and(attributeTable.COLLECTION_ID.eq(collectionId)).fetchOneInto(int.class);
+                } else {
+                    count = context.selectCount().from(attributeTable).where(attributeTable.NAME.eq(name)).and(attributeTable.COLLECTION_ID.eq(this.collectionId)).and(attributeTable.ATTRIBUTE_ID.ne(attributeId)).fetchOneInto(int.class);
+                }
+                if (count > 0) {
+                    validatable.error(new ValidationError(this, "duplicated"));
+                }
             }
         }
     }
