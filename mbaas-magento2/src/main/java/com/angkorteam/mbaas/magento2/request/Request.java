d3a5e33249a67cb0f8ef9900c15964d55bb80091
Socheat KHAUV <pkayjava@gmail.com>
2016-04-12 19:48:26 +0700

    clean mess source

--- mbaas-magento2/src/main/java/com/angkorteam/mbaas/magento2/request/Request.java
+++
@@ -1,7 +0,0 @@
-package com.angkorteam.mbaas.magento2.request;
-
-/**
- * Created by socheat on 2/24/16.
- */
-public class Request {
-}
--- mbaas-magento2/src/main/java/com/angkorteam/mbaas/magento2/response/Response.java
+++
@@ -1,7 +0,0 @@
-package com.angkorteam.mbaas.magento2.response;
-
-/**
- * Created by socheat on 2/24/16.
- */
-public class Response {
-}
