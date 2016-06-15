38d9701dbc414b60545433ce55610dace865feea
Socheat KHAUV <pkayjava@gmail.com>
2016-06-15 23:06:15 +0700

    owner_user_id

--- mbaas-server/src/main/webapp/WEB-INF/application/V4__collection_ddl.sql
+++ mbaas-server/src/main/webapp/WEB-INF/application/V4__collection_ddl.sql
@@ -1,11 +1,11 @@
 CREATE TABLE `collection` (
 
-  collection_id             VARCHAR(100) NOT NULL,
-  name                      VARCHAR(255) NOT NULL,
-  application_code          VARCHAR(100) NOT NULL,
-  locked                    BIT(1)       NOT NULL DEFAULT 0,
-  system                    BIT(1)       NOT NULL DEFAULT 0,
-  owner_user_id VARCHAR(100),
+  collection_id    VARCHAR(100) NOT NULL,
+  name             VARCHAR(255) NOT NULL,
+  application_code VARCHAR(100) NOT NULL,
+  locked           BIT(1)       NOT NULL DEFAULT 0,
+  system           BIT(1)       NOT NULL DEFAULT 0,
+  owner_user_id    VARCHAR(100),
 
   UNIQUE KEY (name),
   INDEX (locked),
