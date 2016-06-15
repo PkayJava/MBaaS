10ea020826eb02387013f47132c815126c2ecf10
Socheat KHAUV <pkayjava@gmail.com>
2016-06-15 23:05:07 +0700

    owner_user_id

--- mbaas-server/src/main/webapp/WEB-INF/application/V4__collection_ddl.sql
+++ mbaas-server/src/main/webapp/WEB-INF/application/V4__collection_ddl.sql
@@ -5,12 +5,12 @@ CREATE TABLE `collection` (
   application_code          VARCHAR(100) NOT NULL,
   locked                    BIT(1)       NOT NULL DEFAULT 0,
   system                    BIT(1)       NOT NULL DEFAULT 0,
-  owner_application_user_id VARCHAR(100),
+  owner_user_id VARCHAR(100),
 
   UNIQUE KEY (name),
   INDEX (locked),
   INDEX (application_code),
   INDEX (system),
-  INDEX (owner_application_user_id),
+  INDEX (owner_user_id),
   PRIMARY KEY (collection_id)
 );
