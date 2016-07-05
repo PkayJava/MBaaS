9507060fd7dd774f81bec36a4d3830e6224bfd46
Socheat KHAUV <pkayjava@gmail.com>
2016-07-05 19:43:03 +0700

    fixed javascript create and modify page

--- mbaas-server/src/main/java/com/angkorteam/mbaas/server/page/javascript/JavascriptCreatePage.java
+++ mbaas-server/src/main/java/com/angkorteam/mbaas/server/page/javascript/JavascriptCreatePage.java
@@ -9,6 +9,8 @@ import com.angkorteam.mbaas.server.validator.JavascriptPathValidator;
 import com.angkorteam.mbaas.server.wicket.MasterPage;
 import com.angkorteam.mbaas.server.wicket.Mount;
 import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
+import org.apache.wicket.markup.head.CssHeaderItem;
+import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.model.PropertyModel;
@@ -46,6 +48,12 @@ public class JavascriptCreatePage extends MasterPage {
     @Override
     public String getPageHeader() {
         return "Create New Javascript";
+    }
+
+    @Override
+    public void renderHead(IHeaderResponse response) {
+        super.renderHead(response);
+        response.render(CssHeaderItem.forCSS(".CodeMirror-fullscreen {padding-left:230px !important; padding-top:50px !important;}", "CodeMirror-fullscreen"));
     }
 
     @Override
