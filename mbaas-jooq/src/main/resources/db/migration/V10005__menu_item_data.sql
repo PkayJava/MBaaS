819622e160d609b06f0e5c2ae87164376aa5a367
Socheat KHAUV <pkayjava@gmail.com>
2016-12-09 13:38:32 +0700

    change icon

--- mbaas-jooq/src/main/resources/db/migration/V10005__menu_item_data.sql
+++ mbaas-jooq/src/main/resources/db/migration/V10005__menu_item_data.sql
@@ -1,8 +1,8 @@
 INSERT INTO menu_item (menu_item_id, section_id, menu_id, title, system, icon, `order`, page_id)
 VALUES
-  (1, NULL, 1, 'Section', TRUE, 'fa-list-ul', 1, 'com.angkorteam.mbaas.server.page.section.SectionBrowsePage'),
+  (1, NULL, 1, 'Section', TRUE, 'fa-leaf', 1, 'com.angkorteam.mbaas.server.page.section.SectionBrowsePage'),
   (2, NULL, 1, 'Menu', TRUE, 'fa-list-ul', 2, 'com.angkorteam.mbaas.server.page.menu.MenuBrowsePage'),
-  (3, NULL, 1, 'Menu Item', TRUE, 'fa-list-ul', 3, 'com.angkorteam.mbaas.server.page.menuitem.MenuItemBrowsePage'),
+  (3, NULL, 1, 'Menu Item', TRUE, 'fa-home', 3, 'com.angkorteam.mbaas.server.page.menuitem.MenuItemBrowsePage'),
   (4, NULL, 1, 'Layout', TRUE, 'fa-columns', 4, 'com.angkorteam.mbaas.server.page.layout.LayoutBrowsePage'),
   (5, NULL, 1, 'Page', TRUE, 'fa-text-height', 5, 'com.angkorteam.mbaas.server.page.page.PageBrowsePage'),
   (6, NULL, 2, 'Role', TRUE, 'fa-key', 1, 'com.angkorteam.mbaas.server.page.role.RoleBrowsePage'),
