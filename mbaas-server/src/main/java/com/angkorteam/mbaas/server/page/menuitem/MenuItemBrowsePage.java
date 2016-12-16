637d70ee8695bc295c73cb1d105ca2d6e60653a5
Socheat KHAUV <pkayjava@gmail.com>
2016-12-16 08:38:24 +0700

    allow to delete menu item

--- mbaas-server/src/main/java/com/angkorteam/mbaas/server/page/menuitem/MenuItemBrowsePage.java
+++ mbaas-server/src/main/java/com/angkorteam/mbaas/server/page/menuitem/MenuItemBrowsePage.java
@@ -3,6 +3,7 @@ package com.angkorteam.mbaas.server.page.menuitem;
 import com.angkorteam.framework.extension.wicket.extensions.markup.html.repeater.data.table.DataTable;
 import com.angkorteam.framework.extension.wicket.extensions.markup.html.repeater.data.table.DefaultDataTable;
 import com.angkorteam.framework.extension.wicket.extensions.markup.html.repeater.data.table.filter.*;
+import com.angkorteam.mbaas.server.Spring;
 import com.angkorteam.mbaas.server.page.MBaaSPage;
 import com.angkorteam.mbaas.server.provider.MenuItemProvider;
 import com.google.common.collect.Maps;
@@ -14,6 +15,7 @@ import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +25,8 @@ import java.util.Map;
  * Created by socheat on 10/24/16.
  */
 public class MenuItemBrowsePage extends MBaaSPage {
+
+    private DataTable<Map<String, Object>, String> dataTable;
 
     @Override
     public String getPageUUID() {
@@ -48,7 +52,7 @@ public class MenuItemBrowsePage extends MBaaSPage {
         columns.add(new TextFilterColumn(provider, ItemClass.Boolean, Model.of("system"), "system", this::modelValue));
         columns.add(new ActionFilterColumn(Model.of("action"), this::actions, this::clickable, this::itemCss, this::itemClick));
 
-        DataTable<Map<String, Object>, String> dataTable = new DefaultDataTable<>("table", columns, provider, 20);
+        dataTable = new DefaultDataTable<>("table", columns, provider, 20);
         dataTable.addTopToolbar(new FilterToolbar(dataTable, filterForm));
         filterForm.add(dataTable);
 
@@ -62,19 +66,27 @@ public class MenuItemBrowsePage extends MBaaSPage {
     private Map<String, IModel<String>> actions() {
         Map<String, IModel<String>> actions = Maps.newHashMap();
         actions.put("Edit", Model.of("Edit"));
+        actions.put("Delete", Model.of("Delete"));
         return actions;
     }
 
-    private void itemClick(String link, Map<String, Object> object, AjaxRequestTarget ajaxRequestTarget) {
-        String menuItemId = (String) object.get("menuItemId");
-        PageParameters parameters = new PageParameters();
-        parameters.add("menuItemId", menuItemId);
-        setResponsePage(MenuItemModifyPage.class, parameters);
+    private void itemClick(String link, Map<String, Object> object, AjaxRequestTarget target) {
+        if ("Edit".equals(link)) {
+            String menuItemId = (String) object.get("menuItemId");
+            PageParameters parameters = new PageParameters();
+            parameters.add("menuItemId", menuItemId);
+            setResponsePage(MenuItemModifyPage.class, parameters);
+        } else if ("Delete".equals(link)) {
+            JdbcTemplate jdbcTemplate = Spring.getBean(JdbcTemplate.class);
+            String menuItemId = (String) object.get("menuItemId");
+            jdbcTemplate.update("DELETE FROM menu_item WHERE menu_item_id = ?", menuItemId);
+            target.add(this.dataTable);
+        }
     }
 
     private Boolean clickable(String link, Map<String, Object> object) {
         Boolean system = (Boolean) object.get("system");
-        if ("Edit".equals(link)) {
+        if ("Edit".equals(link) || "Delete".equals(link)) {
             return !system;
         }
         return false;
@@ -83,6 +95,8 @@ public class MenuItemBrowsePage extends MBaaSPage {
     private ItemCss itemCss(String link, Map<String, Object> model) {
         if ("Edit".equals(link)) {
             return ItemCss.INFO;
+        } else if ("Delete".equals(link)) {
+            return ItemCss.DANGER;
         }
         return ItemCss.NONE;
 
